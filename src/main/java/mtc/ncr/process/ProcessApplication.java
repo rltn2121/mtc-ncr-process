f09d4f3db19f520b4ca3144a959823af38dd0769
박기수 <rltn2121@naver.com>
2023-11-12 16:05:33 +0900

    project setting

---
+++ src/main/java/mtc/ncr/process/ProcessApplication.java
@@ -0,0 +1,12 @@
+package mtc.ncr.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessApplication.class, args);
+    }
+}
+
