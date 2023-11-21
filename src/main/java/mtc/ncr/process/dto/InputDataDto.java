4e787e2395ae6557e9999bd72a24773edadc9075
Subin Bae <sbsoobin@naver.com>
2023-11-22 02:05:52 +0900

    result에서 gid 값 가져오기

---
+++ src/main/java/mtc/ncr/process/dto/InputDataDto.java
@@ -0,0 +1,25 @@
+package mtc.ncr.process.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class InputDataDto {
+    private String gid;
+    private int sno;
+    private String acno;
+    private String trxdt;
+    private String curC;
+    private int upmuG;
+    private String aprvSno;
+    private Double trxAmt;
+    private Double nujkJan;
+    private String errMsg;
+}
--- src/main/java/mtc/ncr/process/queue/DBProcessKafka.java
+++ src/main/java/mtc/ncr/process/queue/DBProcessKafka.java
@@ -2,6 +2,7 @@ package mtc.ncr.process.queue;
 
 import lombok.RequiredArgsConstructor;
 import mtc.ncr.process.dto.GojeongDto;
+import mtc.ncr.process.dto.InputDataDto;
 import mtc.ncr.process.dto.LogDto;
 import mtc.ncr.process.repository.SdtGojeongSlvRepository;
 import org.slf4j.Logger;
@@ -22,21 +23,23 @@ public class DBProcessKafka {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     @KafkaListener(topics = "mtc.dbs.insertChlGojeong", groupId = "amugurnahasam")
-    public void consumeMessage(@Payload GojeongDto data,
+    public void consumeMessage(@Payload InputDataDto data,
                                @Header(name = KafkaHeaders.RECEIVED_KEY, required = false) String key,
                                @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
                                @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timestamp,
                                @Header(KafkaHeaders.OFFSET) long offset) throws SQLException {
         log.info("listener --- [{}][{}][{}][{}][{}]", topic, timestamp, offset, key, data.toString());
-        // gid 를 result에서부터 받아와야함
-        LogDto logDto = new LogDto("{gid}", 1, "prc", data.toString(), "");
-        // log 큐에 넣기
-        kafkaTemplate.send("mtc.ncr.log", "{gid}" , logDto);
 
-        // sno 만들어서 넣기
+        // sno 에 들어가는 값?
+        LogDto logDto = new LogDto(data.getGid(), 3, "prc", data.toString(), "");
+        // log 큐에 넣기
+        kafkaTemplate.send("mtc.ncr.log", data.getGid() , logDto);
+
+        // sno 만들어서 DB에 넣기
         int nxtSno = repository.getMaxSno(data.getAcno()) + 1;
         System.out.println("@@@@@ nxtSno: " + nxtSno);
         GojeongDto gojeongdto = new GojeongDto(nxtSno, data.getAcno(), data.getTrxdt(), data.getCurC(), data.getUpmuG(), data.getAprvSno(), data.getTrxAmt(), data.getNujkJan(), data.getErrMsg());
+
         repository.insertChannel(gojeongdto);
     }
 }
